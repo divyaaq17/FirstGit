8482aa55ddbc9e8245ea06e88027a4a632321595
divyaaq17 <147229380+divyaaq17@users.noreply.github.com>
2023-10-11 23:15:48 +0530

    Create app.js
    DOM assignment 3

---
+++ app.js
@@ -0,0 +1,10 @@
+//console.dir(document)
+
+var headerTitle = document.getElementById("header-title");
+var header =  document.getElementById('main-header');
+
+header.style.borderBottom='solid 10px #000'
+
+var addItemTitle=document.getElementById("add-item-title");
+addItemTitle.style.fontWeight='bold';
+addItemTitle.style.color='green'
